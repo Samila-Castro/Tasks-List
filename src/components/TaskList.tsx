9a7038f17f8d0db1b3623541a27deb32d5f95cd3
Samila Castro <samilav1997@gmail.com>
2021-10-29 11:50:02 -0300

    Adição de task, checked e remove

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -23,17 +23,30 @@ export function TaskList() {
       isComplete: false
     }
 
+    // const oldState = tasks;
+    // setTasks([...oldState,newTask ]);
+
     setTasks(oldState => [...oldState, newTask]);
+  
+    
     setNewTaskTitle('');
     
   }
 
   function handleToggleTaskCompletion(id: number) {
-    // Altere entre `true` ou `false` o campo `isComplete` de uma task com dado ID
+    const newTasks = tasks.map(task => task.id == id ? {
+      ...task,
+      isComplete: !task.isComplete
+    } : task);
+
+    setTasks(newTasks);
   }
 
   function handleRemoveTask(id: number) {
-    // Remova uma task da listagem pelo ID
+    const filteredTasks = tasks.filter(task => task.id !== id);
+
+    setTasks(filteredTasks);
+
   }
 
   return (
